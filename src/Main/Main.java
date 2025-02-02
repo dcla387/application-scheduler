400b7ac3d0a2ac6756ff859354ba76f1b76f7bff
dcla387 <dcla387@wgu.edu>
2025-02-01 19:25:20 -0700

    First go

---
+++ src/Main/Main.java
@@ -0,0 +1,27 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import sample.JDBC;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Welcome Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        JDBC.makeConnection();
+        launch(args);
+    }
+}
