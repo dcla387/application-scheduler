3537f19f1c1b73448fb5b1cbf47e475efd2139e2
dcla387 <dcla387@wgu.edu>
2025-02-02 15:40:54 -0700

    First go

--- src/controller/mainController.java
+++ src/controller/mainController.java
@@ -1,4 +1,34 @@
 package controller;
 
-public class mainController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mainController implements Initializable {
+
+    @FXML
+    private Button mainAppointments;
+
+    @FXML
+    private Button mainCustomers;
+
+    @FXML
+    private Button mainReports;
+
+    @FXML
+    private Button mainExit;
+
+    public void onActionExit(ActionEvent event) {System.exit(0);}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+
+    }
+
 }
