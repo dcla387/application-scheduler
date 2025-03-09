7d9004624a6c95595f32122c2c73ff7f83f62364
dcla387 <dcla387@wgu.edu>
2025-03-09 00:54:04 -0700

    First go

--- src/controller/modCustController.java
+++ src/controller/modCustController.java
@@ -1,11 +1,14 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +16,27 @@ import java.util.ResourceBundle;
 import java.io.IOException;
 
 public class modCustController implements Initializable {
+
+    @FXML
+    private TextField addCustIDTextField;
+
+    @FXML
+    private TextField addCustNameTextField;
+
+    @FXML
+    private TextField addCustAddress1TextField;
+
+    @FXML
+    private TextField addCustomerPhoneTextField;
+
+    @FXML
+    private ComboBox<String> addCustCountryTextField;
+
+    @FXML
+    private ComboBox<String> addCustStateTextField;
+
+    @FXML
+    private TextField addCustZipTextField;
 
     public void onClickToModCustomer(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/CustMain.fxml"));
