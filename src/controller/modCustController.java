c4bb8971662240425e9705025e0814da87492bcd
dcla387 <dcla387@wgu.edu>
2025-03-09 00:42:53 -0700

    First go

--- src/controller/modCustController.java
+++ src/controller/modCustController.java
@@ -2,14 +2,17 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import java.io.IOException;
 
-public class modCustController {
+public class modCustController implements Initializable {
 
     public void onClickToModCustomer(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/CustMain.fxml"));
@@ -31,5 +34,10 @@ public class modCustController {
         stage.setTitle("Main Cust Page");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
