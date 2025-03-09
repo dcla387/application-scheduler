1a69a3361836bf4b4db0c2a42245fb917a059993
dcla387 <dcla387@wgu.edu>
2025-03-09 14:06:35 -0600

    First go

--- src/Model/Division.java
+++ src/Model/Division.java
@@ -2,6 +2,25 @@ package Model;
 
 public class Division {
 
+    private int divisionId;
+
+    private String name;
+
+    private int countryId;
+
+
     public Division(int divisionId, String name, int countryId) {
+        this.divisionId = divisionId;
+        this.name = name;
+        this.countryId = countryId;
     }
+
+    public int getDivisionID() { return divisionId;}
+    public String getName() { return name;}
+    public int getCountryId() { return countryId;}
+
+
+
+    @Override
+    public String toString() {return name;}
 }
