257710882e678aaff8d0ba5e28479580dc7be4c3
dcla387 <dcla387@wgu.edu>
2025-03-16 15:54:11 -0600

    First go

--- src/controller/addApptController.java
+++ src/controller/addApptController.java
@@ -2,6 +2,7 @@ package controller;
 
 import DAO.CustomerDAO;
 import Model.Customer;
+import Model.Appointment;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,12 +11,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.net.URL;
 import javafx.collections.ObservableList;
+import org.w3c.dom.Text;
+
 import java.util.ResourceBundle;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import java.awt.*;
 import java.io.IOException;
@@ -24,27 +31,44 @@ import java.io.IOException;
 public class addApptController implements Initializable {
 
     @FXML
-    private TextField customerSearchField;
+    private ComboBox<Customer> customerComboBox;
 
     @FXML
-    private TextField customerIdField;
+    private TextField apptIDTextField;
 
-    private ObservableList<Customer> allCustomers;
-    private Customer selectedCustomer;
+    @FXML
+
+    private TextField titleTextField;
+
+    @FXML
+    private TextField descriptionTextField;
+
+    @FXML
+    private TextField locationTextField;
+
+    @FXML
+    private ComboBox<Contact> contactComboBox;
+
+    @FXML
+    private TextField typeTextField;
+
+    @FXML
+    private DatePicker startDatePicker;
+
+    @FXML
+    private ComboBox<String> endTimeComboBox;
+
+    @FXML
+    private TextField userIDTextField;
+
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        allCustomers = CustomerDAO.getAllCustomers();
-        customerSearchField.textProperty().addListener((observable, oldValue,newValue) -> {
-            findCustomerByName(newValue);
-        });
-        }
-
-        private void findCustomerByName(String name) {
-        if (name.isEmpty()) {
-            customerIdField.clear();
-            selectedCustomer = null;
-            return;
+        customerComboBox.setItems(CustomerDAO.getAllCustomers());
+        contactComboBox.setItems(ContactDAO.getAllContacts());
+        populateTimeComboBoxes();
         }
 
         String searchName = name.toLowerCase().trim();
