cfe704e780c96f88d198959c55aee8170349586b
dcla387 <dcla387@wgu.edu>
2025-04-03 00:38:38 -0600

    First go

--- src/Model/Division.java
+++ src/Model/Division.java
@@ -1,12 +1,35 @@
 package Model;
 
+/**
+ * this class encapsulates division information including unique ID's and the country it belongs to.
+ *
+ * @author Donna Clark
+ * @version 1.0
+ */
+
 public class Division {
 
+    /**
+     * Unique ID for the division
+     */
     private int divisionId;
+    /**
+     * Name of the division
+     */
 
     private String name;
+    /**
+     * ID of the country to which the division belongs
+     */
 
     private int countryID;
+
+    /**
+     * constructs a new Division with the specified info.
+     * @param divisionId Unique ID for the division
+     * @param name Name of the Division
+     * @param countryID ID of the country assoicated with the Division
+     */
 
 
     public Division(int divisionId, String name, int countryID) {
@@ -15,14 +38,27 @@ public class Division {
         this.countryID = countryID;
     }
 
+    /**
+     * Retrieves the name of the division
+     * @return division's name as a string
+     */
+
 
     public String getName() { return name;}
     public int getCountryID() { return countryID;}
 
-
+    /**
+     * Returns a string representation of the division
+     * @return the divisiong's name
+     */
 
     @Override
     public String toString() {return name;}
+
+    /**
+     * Retrieves the unique ID of the division
+     * @return Division's ID as an INT
+     */
 
     public int getDivisionId() {
         return divisionId;
