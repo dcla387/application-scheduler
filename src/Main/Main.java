2058a28922b4265dc5ee7454c398e35f20d6735b
dcla387 <dcla387@wgu.edu>
2025-04-02 23:41:17 -0600

    First go

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,9 +6,29 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import DAO.JDBC;
 
-/**This is an application that holds customers and their appointments */
+/**
+ * Main application class for the customer and appointment management system.
+ *
+ * <p>This class serves as the entry point for the application. The application allows users
+ * to manage customer records and their associated appointment information.</p>
+ *
+ * <p>The application uses JavaFX for the user interface and JDBC for database connectivity.
+ * The initial view presented to the user is the login screen.</p>
+ *
+ * @author Donna Clark
+ * @version 1.0
+ */
 
 public class Main extends Application {
+
+    /**
+     * Initiailizes and displays the primary stage of the JavaFX application.
+     *
+     * <p>this method loads the login screen, creates a new scene wit hthe loaded content, displays the stage to the User.</p>
+     *
+     * @param stage The primary stage for this application
+     * @throws Exception If an error occurs in the loading process
+     */
 
     @Override
     public void start(Stage stage) throws Exception{
@@ -19,6 +39,13 @@ public class Main extends Application {
         stage.show();
     }
 
+    /**
+     * the Main entry point for the application.
+     *
+     * <p>this method is the one that establishes the database connection and launches the application.  Also closes the connection when application terminates.</p>
+     *
+     * @param args Command line arguments passed to the application
+     */
 
     public static void main(String[] args) {
         JDBC.makeConnection();
