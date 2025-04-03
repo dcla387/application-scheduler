d01adf052b485479dee8b528f579dd9be1919bd2
dcla387 <dcla387@wgu.edu>
2025-04-03 00:17:33 -0600

    First go

--- src/Model/Contact.java
+++ src/Model/Contact.java
@@ -1,8 +1,29 @@
 package Model;
 
+/**
+ * This class holds basic contact information
+ *
+ * @author Donna Clark
+ * @version 1.0
+ */
+
 public class Contact {
+
+    /**
+     * Field stores the contact's unique ID number
+     */
     private int contactId;
+
+    /**
+     * This field stores the contact's name
+     */
     private String contactName;
+
+    /**
+     * Constructs a new contact with the specified ID and name
+     * @param contactId The unique ID for the contact
+     * @param contactName The unique name for the contact
+     */
 
     public Contact(int contactId, String contactName) {
         this.contactId = contactId;
@@ -10,15 +31,27 @@ public class Contact {
 
     }
 
+    /**
+     * Retrieves the contact's ID
+     * @return the contact's ID as an INT
+     */
+
         public int getContactId() {
         return contactId;
         }
 
-        public String getContactName() {
+    /**
+     * Retrieves the contact's name
+     * @return the contact's name
+     */
+    public String getContactName() {
         return contactName;
         }
 
-
+    /**
+     * Returns a strgin representation of the contact
+     * @return the contact's name
+     */
         @Override
     public String toString() {
         return contactName;
